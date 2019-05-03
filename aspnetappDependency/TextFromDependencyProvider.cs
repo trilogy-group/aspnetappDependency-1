6a3c41128d2502c68f44789ff6515c47e3eb65b0
Denis Palnitsky <denis.palnitsky@aurea.com>
2019-05-03 12:40:27 +0200

    Initial commit

---
+++ aspnetappDependency/TextFromDependencyProvider.cs
@@ -0,0 +1,12 @@
+﻿using System;
+
+namespace aspnetappDependency
+{
+    public class TextFromDependencyProvider
+    {
+        public static string Text { get { return $"This text is from Dependency library \n" +
+                                    $"Name:{typeof(TextFromDependencyProvider).Assembly.GetName().FullName} \n" +
+                                    $"Version: {typeof(TextFromDependencyProvider).Assembly.GetName().Version}\n" +
+                                    $"Location: {typeof(TextFromDependencyProvider).Assembly.Location}"; } }
+    }
+}
